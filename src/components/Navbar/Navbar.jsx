dd06c4a396159a3fef16980f068bba2bd3519553
mygitaza <mongss122@hotmail.com>
2025-04-30 12:07:32 +0100

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import './Navbar.css'
+import { useDispatch } from 'react-redux'
+import { useLogoutUserMutation } from '../../redux/feature/user/userApi';
+import { logOut } from '../../redux/feature/slice/authSlice';
 
 const Navbar = () => {
+  const [logoutUser] = useLogoutUserMutation();
+      const dispatch = useDispatch();
+  
+      const handleLogout = async () =>{
+          const confirmLogout = window.confirm("Are you sure you want to log out?");
+          if(!confirmLogout) return;
+          try {
+              await logoutUser().unwrap();
+              dispatch(logOut());
+          } catch (error) {
+              console.error(error, 'Failed to logout')
+          }
+      }
   return (
     <header className='admin-header'>
         <nav className="admin-header-wrapper">
         <h3>Admin Panel</h3>
-        <button>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
         </nav>
     </header>
   )
