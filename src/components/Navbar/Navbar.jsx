2b4393e3fef60fce129110a446a07ba0fdd241cc
mygitaza <mongss122@hotmail.com>
2025-04-30 14:14:21 +0100

    fix logout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import './Navbar.css'
 import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { useLogoutUserMutation } from '../../redux/feature/user/userApi';
 import { logOut } from '../../redux/feature/slice/authSlice';
 
 const Navbar = () => {
   const [logoutUser] = useLogoutUserMutation();
       const dispatch = useDispatch();
+      const navigate= useNavigate()
   
       const handleLogout = async () =>{
-          const confirmLogout = window.confirm("Are you sure you want to log out?");
-          if(!confirmLogout) return;
-          try {
-              await logoutUser().unwrap();
-              dispatch(logOut());
-          } catch (error) {
-              console.error(error, 'Failed to logout')
+              const confirmLogout = window.confirm("Are you sure you want to log out?");
+              if(!confirmLogout) return;
+              try {
+                  await logoutUser().unwrap();
+                  dispatch(logOut());
+                  localStorage.removeItem("user");
+                  localStorage.removeItem("token");
+                  navigate("/")
+              } catch (error) {
+                  console.error(error, 'Failed to logout')
+              }
           }
-      }
   return (
     <header className='admin-header'>
         <nav className="admin-header-wrapper">
